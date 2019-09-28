1764463718c9c2768987e9832c647af86e6748dd
Andreas Rohleder <andreas@rohleder.cc>
2019-09-28 12:28:07 +0200

    Moved to github

---
+++ Cave.Registry/RegistrySettingAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace Cave
+{
+    /// <summary>
+    /// Attribute for registry settings options.
+    /// </summary>
+    public class RegistrySettingAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the used value kind.
+        /// </summary>
+        public RegistryValueKind ValueKind { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether obfuscate the data to make it a little
+        /// harder to read.
+        /// This can only be used with <see cref="ValueKind"/> == <see cref="RegistryValueKind.Binary"/>.
+        /// </summary>
+        public bool Obfuscate { get; set; }
+    }
+}
